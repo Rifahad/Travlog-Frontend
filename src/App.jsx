9d24bdc55bcfce8240c490ecc2806074b1078ef1
rifahad <rifahadmt202@gmail.com>
2024-06-06 12:55:39 +0530

    Navbar created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from './Components/Navbar'
+import Section1 from "./Components/Section1";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Section1 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Section1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div className='w-full h-screen bg-yellow-50'>
+      <div className='w-full h-2/3 bg-black'> 
+
+      </div>
+    </div>
+  )
+}
+
+export default Section1
